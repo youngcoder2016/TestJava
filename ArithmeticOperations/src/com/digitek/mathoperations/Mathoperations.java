eceba74ad5036fdb5afbdfe77dd425f51b415245
mori15 <mms@MMSs-MacBook-Pro.local>
2016-01-17 20:36:25 -0600

    checking my first version
    checking my first version

---
+++ ArithmeticOperations/src/com/digitek/mathoperations/Mathoperations.java
@@ -0,0 +1,22 @@
+package com.digitek.mathoperations;
+
+public class Mathoperations {
+	
+	public static void addNumbers(){
+		int a = 15;
+		int b = 10;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+	public static void subtractNumbers() {
+		int a = 20;
+		int b = 10;
+		int c = a-b;
+		System.out.println(c);
+	}
+	public static void main(String args[]){
+		addNumbers();
+		subtractNumbers();
+	}
+}
